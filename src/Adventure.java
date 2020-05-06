4c65f1d76f93250384858185541db6d807eb3f65
Esai Flores <flores.esaijonathan@gmail.com>
2020-05-06 16:40:37 -0500

    fixed spacing

--- src/Adventure.java
+++ src/Adventure.java
@@ -1,34 +1,39 @@
 import java.util.Scanner;
 
-
 public class Adventure {
 
-    Scanner myScanner = new Scanner(System.in);
-    int playerHP;
-    String playerWeapon;
-    String playerName;
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
 
         startGame();
 
     }
 
-    public void startGame() {
+
+
+
+
+    public static void startGame() {
+
+        Scanner myScanner = new Scanner(System.in);
+        int playerHP;
+        String playerWeapon;
+        String playerName;
+
 
         playerHP = 10;
         playerWeapon = "Blaster";
 
-        System.out.println("Player HP" + playerHP);
-        System.out.println("Player Weapon" + playerWeapon);
+        System.out.println("Player HP " + playerHP);
+        System.out.println("Player Weapon " + playerWeapon);
 
 
 
-        System.out.println("What is your name ?");
+        System.out.println("What is your name? ");
 
         playerName = myScanner.nextLine();
 
-        System.out.println("Are you ready to start the game" + playerName + "?" );
+        System.out.println("Are you ready to start the game " + playerName + "?" );
 
 
 
